a77812577459460bffc894dd202994374111fbea
EvgeniyKarpuhin <81818834+EvgeniyKarpuhin@users.noreply.github.com>
2022-09-01 23:29:07 +0300

    add

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
-export const TodoForm: React.FC = () => {
-    
+interface TodoFormProps {
+    onAdd(title: string): void
+}
+
+export const TodoForm: React.FC<TodoFormProps> = props => {
+    const ref = useRef<HTMLInputElement>(null)
+
+    const keyPressHandler = (event: React.KeyboardEvent) => {
+        if (event.key === 'Enter') {
+            props.onAdd(ref.current!.value)
+            ref.current!.value = ''
+        }
+    }
+
     return (
         <div className='input-field mt2'>
-            <input type='text' id='title' placeholder='Введите называние дела'/>
+            <input 
+            ref={ref}
+            type='text' 
+            id='title' 
+            placeholder='Введите называние дела'
+            onKeyPress={keyPressHandler}/>
             <label htmlFor='title' className='active'>
                 Введите название дела
             </label>
