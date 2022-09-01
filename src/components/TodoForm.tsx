27c98159085b4fd2c55842b7d879ff5d9dbcb1d1
EvgeniyKarpuhin <81818834+EvgeniyKarpuhin@users.noreply.github.com>
2022-09-01 22:01:58 +0300

    add

---
+++ src/components/TodoForm.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const TodoForm: React.FC = () => {
+    
+    return (
+        <div className='input-field mt2'>
+            <input type='text' id='title' placeholder='Введите называние дела'/>
+            <label htmlFor='title' className='active'>
+                Введите название дела
+            </label>
+        </div>
+    )
+}
