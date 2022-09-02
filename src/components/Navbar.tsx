a5a9cf28cf6088bb051d20858a208cd6a627a102
EvgeniyKarpuhin <81818834+EvgeniyKarpuhin@users.noreply.github.com>
2022-09-03 00:02:53 +0300

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,7 @@ export const Navbar: React.FC = () => (
       <a href="/" className="brand-logo">React + TypeScript</a>
       <ul className="right hide-on-med-and-down">
         <li><a href="/">Список дел</a></li>
-        <li><a href="/">О нас</a></li>
+        <li><a href="/">Информация</a></li>
       </ul>
     </div>
   </nav>
